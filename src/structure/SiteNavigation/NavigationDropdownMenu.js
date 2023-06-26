0d6f8588e602cd29a1ebbea8f3abe6d50d1f0d68
jon-grant-state-ca-gov <jon.grant@state.ca.gov>
2023-06-26 15:48:30 -0700

    Swap button for anchor to fix dropdown issue

--- src/structure/SiteNavigation/NavigationDropdownMenu.js
+++ src/structure/SiteNavigation/NavigationDropdownMenu.js
@@ -24,19 +24,21 @@ const NavigationDropdownMenu = ({ contentArr }) => {
         {contentArr.map((item, idx) => (
           <li className="nav-item" key={`ndm-list-item${idx}`}>
 
-            {item.href ? <a className={'first-level-link'} href={item.href}>{item.navHeader}</a> :
+            {item.href ?
+              <a className={'first-level-link'} href={item.href}>{item.navHeader}</a> :
               <>
                 <div className={'has-sub-btn'}>
-                  <button onClick={handleToggle} id={`navheader_tab${idx}`} className={'first-level-btn nav-header has-sub'} aria-expanded={false} aria-controls={`navheader_panel${idx}`} data-nav-id={''}>
+                  {/*eslint-disable-next-line jsx-a11y/anchor-is-valid*/}
+                  <a onClick={handleToggle} id={`navheader_tab${idx}`} className={'first-level-btn nav-header has-sub'} aria-expanded={false} aria-controls={`navheader_panel${idx}`} data-nav-id={''}>
                     {item.navHeader}
-                  </button>
+                  </a>
                 </div>
 
                 <div className="sub-nav" id={`navheader_panel${idx}`} role={'tabpanel'} aria-labelledby={`navheader_tab${idx}`} data-nav-id={''} aria-hidden={true}>
                   <ul className="second-level-nav">
                     {item.navBodyArr.map((navItem, navIdx) => (
-                      <li className="unit1" key={`ndm-nav-list-item${navIdx}`}>
-                        <a href={navItem.href} className="second-level-link" tabIndex={-1}>{navItem.title}</a>
+                      <li className={"unit1"} key={`ndm-nav-list-item${navIdx}`} id={`ndm-nav-list-item${navIdx}`}>
+                        <a href={navItem.href} className={"second-level-link"} tabIndex={-1}>{navItem.title}</a>
                       </li>
                     ))}
                   </ul>
