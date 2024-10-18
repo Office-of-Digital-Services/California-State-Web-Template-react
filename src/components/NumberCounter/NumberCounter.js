39537d420efb15dda88a012b8c3f4be23d9225bf
jon-grant-state-ca-gov <jon.grant@state.ca.gov>
2024-10-18 14:24:24 -0700

    Update to use State Template 6.3.2, move NumberCounter logic to component

--- src/components/NumberCounter/NumberCounter.js
+++ src/components/NumberCounter/NumberCounter.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { useEffect } from 'react';
 import PropTypes from 'prop-types';
 import '../../js/cagov/number-counter';
 
@@ -18,6 +19,68 @@ import '../../js/cagov/number-counter';
  */
 
 const NumberCounter = ({ countTime = '4000', targetNumber }) => {
+
+  useEffect(() => {
+    var elements = document.querySelectorAll(".scroll-counter");
+
+    elements.forEach(function (item) {
+      // Add new attributes to the elements with the '.scroll-counter' HTML class
+      item.counterAlreadyFired = false
+      item.counterSpeed = item.getAttribute("data-counter-time") / 45
+      item.counterTarget = +item.innerText
+      item.counterCount = 0
+      item.counterStep = item.counterTarget / item.counterSpeed
+
+      item.updateCounter = function () {
+        item.counterCount = item.counterCount + item.counterStep
+        item.innerText = Math.ceil(item.counterCount)
+
+        if (item.counterCount < item.counterTarget) {
+          setTimeout(item.updateCounter, item.counterSpeed)
+        } else {
+          item.innerText = item.counterTarget
+        }
+      }
+    })
+
+    // Function to determine if an element is visible in the web page
+    var isElementVisible = function isElementVisible(el) {
+      var scroll = window.scrollY || window.pageYOffset
+      var boundsTop = el.getBoundingClientRect().top + scroll
+      var viewport = {
+        top: scroll,
+        bottom: scroll + window.innerHeight,
+      }
+      var bounds = {
+        top: boundsTop,
+        bottom: boundsTop + el.clientHeight,
+      }
+      return (
+        (bounds.bottom >= viewport.top && bounds.bottom <= viewport.bottom) ||
+        (bounds.top <= viewport.bottom && bounds.top >= viewport.top)
+      )
+    }
+
+    // Funciton that will get fired uppon scrolling
+    var handleScroll = function handleScroll() {
+      elements.forEach(function (item, id) {
+        if (true === item.counterAlreadyFired) return;
+        if (!isElementVisible(item)) return;
+        item.updateCounter();
+        item.counterAlreadyFired = true;
+      });
+    }
+
+    // Fire the function on load and scroll
+    window.addEventListener("load", function () {
+      handleScroll();
+    });
+    window.addEventListener("scroll", function () {
+      handleScroll();
+    });
+  }, [])
+
+
   return (
     <div className={'stat-counter text-accent1'}>
       <span className={'scroll-counter'} data-counter-time={countTime} style={{ visibility: 'visible' }}>{targetNumber}</span>
